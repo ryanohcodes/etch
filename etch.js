a9bc8bb5f1cc1e8087f44a9b61711e3d2747f235
Ryan Oh <ryanohnoes@gmail.com>
2021-12-01 22:22:41 -0800

    color changing on mouse hover

--- etch.js
+++ etch.js
@@ -1,6 +1,8 @@
 const div = document.createElement('div');
 const container = document.querySelector('.container')
 div.classList.add('box');
+const button = document.querySelector('button');
+
 
 for (let i = 0; i < 256; i++){
     container.appendChild(div.cloneNode(true));
@@ -9,6 +11,12 @@ for (let i = 0; i < 256; i++){
 const boxes = document.querySelectorAll('.box');
 for (let i=0; i < boxes.length; i++){
     boxes[i].addEventListener('mouseover', () => {
-            div.classList.add('filler');
+            boxes[i].classList.add('filler');
         });
 }
+
+button.addEventListener('click', ()=> {
+    for (let i = 0; i < boxes.length; i++){
+        boxes[i].classList.remove('filler');
+    }
+});
