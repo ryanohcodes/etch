a5036d1f48e0ba68c840248ead2dcaf7b9155477
Ryan Oh <ryanohnoes@gmail.com>
2021-12-02 18:58:10 -0800

    assignment completion, minimum

--- etch.js
+++ etch.js
@@ -3,20 +3,58 @@ const container = document.querySelector('.container')
 div.classList.add('box');
 const button = document.querySelector('button');
 
+let grid = 256;
 
-for (let i = 0; i < 256; i++){
+for (let i = 0; i < grid; i++){
     container.appendChild(div.cloneNode(true));
 }
 
-const boxes = document.querySelectorAll('.box');
+let boxes = document.querySelectorAll('.box');
 for (let i=0; i < boxes.length; i++){
     boxes[i].addEventListener('mouseover', () => {
             boxes[i].classList.add('filler');
         });
 }
 
+colorIn();
+
+function colorIn(){
+    for (let i=0; i < boxes.length; i++){
+    boxes[i].addEventListener('mouseover', () => {
+            boxes[i].classList.add('filler');
+        });
+    }
+}
+
+
 button.addEventListener('click', ()=> {
     for (let i = 0; i < boxes.length; i++){
         boxes[i].classList.remove('filler');
     }
+
+    for (let i = 0; i < boxes.length; i++){
+        container.removeChild(boxes[i]);
+    }
+
+    grid = prompt('Enter desired grid size num x num that is less than 100');
+
+    while (grid > 100){
+        grid = prompt('Enter desired grid size num x num that is less than 100');
+    }
+
+    grid = parseInt(grid);
+    let sizeFactor = 22;
+    let newGrid = grid * grid;
+
+    let newBox = grid * sizeFactor;
+
+    document.getElementById('container').style.width = `${newBox}px`;
+    document.getElementById('container').style.heigth = `${newBox}px`;
+
+    for (let i = 0; i < newGrid; i++){
+        container.appendChild(div.cloneNode(true));
+    }
+    boxes = document.querySelectorAll('.box');
+    colorIn();
+    
 });
